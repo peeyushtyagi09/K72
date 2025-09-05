7fb4e2c28287c06243783010a5705b5db08ae9f5
peeyushtyagi09 <peeyushtyagi82@gmail.com>
2025-09-06 01:40:02 +0530

    fix the Stair thing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import Stair from './components/common/stair.jsx'
+import Stair from './components/common/Stair.jsx'
 import './index.css'
 import App from './App.jsx'
 
